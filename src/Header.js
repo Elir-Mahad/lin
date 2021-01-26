17e58d178bbdb96d722761aa34d8d552feecee01
elir-mahad <mmahad280@gmail.com>
2021-01-26 17:40:47 -0500

    header components

--- src/Header.js
+++ src/Header.js
@@ -2,19 +2,26 @@ import React from "react";
 import "./Header.css";
 //
 import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from "./HeaderOption";
 
 function Header() {
 	return (
 		<div className="header">
 			<div className="header_left">
-				<img src="" alt="" />
+				<img
+					src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+					alt=""
+				/>
 
 				<div className="header_search">
 					<SearchIcon />
 					<input type="text" />
 				</div>
 			</div>
-			<div className="header_right"></div>
+			<div className="header_right">
+				<HeaderOption title="home" />
+				<HeaderOption title="My network" />
+			</div>
 		</div>
 	);
 }
---
+++ src/HeaderOption.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HeaderOption.css";
+
+function HeaderOption(Icon, title) {
+	return (
+		<div className="headerOption">
+			{Icon && <Icon className="heeaderOption_icon" />}
+			{/* if an icon is passed in, then and only then render the icon  */}
+			<h3 className="headerOption_title"> {title} </h3>
+		</div>
+	);
+}
+
+export default HeaderOption;
