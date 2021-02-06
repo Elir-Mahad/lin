6b1cbb0fb21069b4ca8380167251f4a94307ed4d
Elir-Mahad <mmahad280@gmail.com>
2021-02-06 18:43:53 -0500

    back in the game

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./HeaderOption.css";
 
-function HeaderOption(Icon, title) {
+// function HeaderOption(Icon, title) {
+// 	return (
+// 		<div className="headerOption">
+// 			{Icon && <Icon className="heeaderOption_icon" />}
+// 			{/* if an icon is passed in, then and only then render the icon  */}
+// 			<h3 className="headerOption_title"> {title} </h3>
+// 		</div>
+// 	);
+// }
+
+function HeaderOption() {
 	return (
 		<div className="headerOption">
-			{Icon && <Icon className="heeaderOption_icon" />}
+			{/* {Icon && <Icon className="heeaderOption_icon" />} */}
 			{/* if an icon is passed in, then and only then render the icon  */}
-			<h3 className="headerOption_title"> {title} </h3>
+			<h3 className="headerOption_title"> Title </h3>
 		</div>
 	);
 }
