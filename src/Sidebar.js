07f107c763d26138402900e90e0c9a1b650b837e
Elir-mahad <mmahad280@gmail.com>
2021-02-07 12:32:14 -0500

    styled the sidebar component - added background image etc

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,17 +7,22 @@ function Sidebar() {
 	return (
 		<div className="sidebar">
 			<div className="sidebar_top">
-				<img src="" alt="" />
-				<Avatar />
+				<img
+					src="https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+					alt=""
+				/>
+				<Avatar className="sidebar_avatar" />
 				<h2> Mahad </h2>
 				<h4> mahad@gmail.com</h4>
 			</div>
-			<div className="sidebar_stat">
-				<p> Who viewed you</p>
-				<div className="sidebar_statNumber">2,543</div>
+			<div className="sidebar_stats">
+				<div className="sidebar_stat">
+					<p> Who viewed you</p>
+					<div className="sidebar_statNumber">2,543</div>
+				</div>
 				<div className="sidebar_stat">
 					<p> Views on post</p>
-					<p className="sidebare_statNumber"> 2,448</p>
+					<p className="sidebar_statNumber"> 2,448</p>
 				</div>
 			</div>
 			<div className="sidebar_bottom">
