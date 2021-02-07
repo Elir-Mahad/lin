96b51ec9f8db1ee537f773d6f0962e1d4caa3d87
Elir-mahad <mmahad280@gmail.com>
2021-02-07 10:43:51 -0500

    added the headerOption icon

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,22 +1,12 @@
 import React from "react";
 import "./HeaderOption.css";
 
-// function HeaderOption(Icon, title) {
-// 	return (
-// 		<div className="headerOption">
-// 			{Icon && <Icon className="heeaderOption_icon" />}
-// 			{/* if an icon is passed in, then and only then render the icon  */}
-// 			<h3 className="headerOption_title"> {title} </h3>
-// 		</div>
-// 	);
-// }
-
-function HeaderOption() {
+function HeaderOption({ Icon, title }) {
 	return (
 		<div className="headerOption">
-			{/* {Icon && <Icon className="heeaderOption_icon" />} */}
+			{Icon && <Icon className="heeaderOption_icon" />}
 			{/* if an icon is passed in, then and only then render the icon  */}
-			<h3 className="headerOption_title"> Title </h3>
+			<h3 className="headerOption_title"> {title} </h3>
 		</div>
 	);
 }
