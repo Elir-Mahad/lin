271e2e8da6d9f141987e2ab855f0c2d6c35b3662
Elir-mahad <mmahad280@gmail.com>
2021-02-07 11:24:18 -0500

    added the sidebar component and css file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./App.css";
+//
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
+//
 function App() {
 	return (
 		<div className="app">
 			<Header />
-			{/* header */}
-			{/* sidebar */}
-			{/* feed */}
-			{/* widgets */}
+
+			{/* App Body below */}
+			<div className="app_body">
+				<Sidebar />
+				{/* sidebar */}
+				{/* feed */}
+				{/* widgets */}
+			</div>
 		</div>
 	);
 }
