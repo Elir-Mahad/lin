0c4ce9b42891da2f83da08a4fc0c922da3549213
Elir-mahad <mmahad280@gmail.com>
2021-02-07 11:16:06 -0500

    added the avataricon

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,6 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from "@material-ui/icons/Home";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
-
 import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
 import ChatIcon from "@material-ui/icons/Chat";
 import NotificationsIcon from "@material-ui/icons/Notifications";
@@ -31,6 +30,7 @@ function Header() {
 				<HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
 				<HeaderOption Icon={ChatIcon} title="Messaging" />
 				<HeaderOption Icon={NotificationsIcon} title="Notifications" />
+				<HeaderOption avatar="https://lh5.googleusercontent.com/-1CIp2TvQxXY/VBoJ_l8EUwI/AAAAAAAABg4/0VEuW-tXJE8/s640/blogger-image--586450017.jpg" />
 			</div>
 		</div>
 	);
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./HeaderOption.css";
+import { Avatar } from "@material-ui/core";
 
-function HeaderOption({ Icon, title }) {
+function HeaderOption({ avatar, Icon, title }) {
 	return (
 		<div className="headerOption">
 			{Icon && <Icon className="heeaderOption_icon" />}
 			{/* if an icon is passed in, then and only then render the icon  */}
+			{avatar && (
+				<Avatar
+					//
+					className="headerOption_icon"
+					src={avatar}
+				/>
+			)}
 			<h3 className="headerOption_title"> {title} </h3>
 		</div>
 	);
