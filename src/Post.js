2621b9fe36710017721b62af68dc3ebeacbe5341
Elir-mahad <mmahad280@gmail.com>
2021-02-09 07:58:49 -0500

    added 4 icons to the post component by importing the InputOption component into the post component, and then defining the Icon type title and color for each icon in the post_buttons div which is in the post component

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Post.css";
 //
+import InputOption from "./InputOption";
+//
 import { Avatar } from "@material-ui/core";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ChatOutlinedIcon from "@material-ui/icons/ChatOutlined";
+import ShareOutlinedIcon from "@material-ui/icons/ShareOutlined";
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
+
+//
 
 function Post({ name, description, message, photoUrl }) {
 	// the props name, description etc will be passed through this component
@@ -20,6 +28,32 @@ function Post({ name, description, message, photoUrl }) {
 			<div className="post_body">
 				<p>{message}</p>
 				{/* the prop message is passed here */}
+			</div>
+			<div className="post_buttons">
+				<InputOption
+					//
+					Icon={ThumbUpAltOutlinedIcon}
+					title="like"
+					color="gray"
+				/>
+				<InputOption
+					//
+					Icon={ChatOutlinedIcon}
+					title="like"
+					color="gray"
+				/>
+				<InputOption
+					//
+					Icon={ShareOutlinedIcon}
+					title="like"
+					color="gray"
+				/>
+				<InputOption
+					//
+					Icon={SendOutlinedIcon}
+					title="like"
+					color="gray"
+				/>
 			</div>
 		</div>
 	);
